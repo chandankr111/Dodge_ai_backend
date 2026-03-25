b7dab40aefbb5a797376075738ac87ab6833c9b1
chandankr111 <122672735+chandankr111@users.noreply.github.com>
2026-03-25 13:49:33 +0530

    dist folder is fixed

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 import { app } from './app.js';
-const PORT = Number(process.env.PORT) || 3001;
+const PORT = Number(process.env.PORT) || 8000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
